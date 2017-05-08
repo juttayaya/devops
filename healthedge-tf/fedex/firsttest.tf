b33b791dee6b6df2bc16c85f332344edbe4e5d06
Jirawat Uttayaya <admin@apps.jirawat.com>
2017-05-08 14:42:25 -0400

    Add security group

--- healthedge-tf/fedex/firsttest.tf
+++ healthedge-tf/fedex/firsttest.tf
@@ -5,9 +5,21 @@ provider "aws" {
 resource "aws_instance" "he-fedex" {
   ami = "ami-6d1c2007"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.he-sg.id}"]
 
   tags {
     Name = "healthedge-centos7"
   }
 }
+
+resource "aws_security_group" "he-sg" {
+  name = "healthedge-centos7-instance"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 
