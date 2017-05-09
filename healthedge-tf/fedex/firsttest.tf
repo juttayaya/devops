384cb97d317cdc4a21629fdc362028acaf62f297
Jirawat Uttayaya <admin@apps.jirawat.com>
2017-05-09 11:20:20 -0400

    Create before destroy

--- healthedge-tf/fedex/firsttest.tf
+++ healthedge-tf/fedex/firsttest.tf
@@ -7,18 +7,29 @@ resource "aws_instance" "he-fedex" {
   instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.he-sg.id}"]
   key_name = "he-keypair"
+
   tags {
     Name = "he-centos7"
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
 }
 
 resource "aws_security_group" "he-sg" {
   name = "he-centos7-sg"
+
   ingress {
     from_port = 22
     to_port = 22
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
