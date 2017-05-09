7609b0d3e6e4eae05dc3ddfde930c4b5cb01cdf7
Jirawat Uttayaya <admin@apps.jirawat.com>
2017-05-09 12:43:43 -0400

    AMI ID variable

--- healthedge-tf/fedex/firsttest.tf
+++ healthedge-tf/fedex/firsttest.tf
@@ -2,8 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "ami_id" {
+  description = "AMI ID to use to build Connector VM"
+  default = "ami-0a41351c"
+}
+
 resource "aws_instance" "he-fedex" {
-  ami = "ami-5850244e"
+  ami = "${var.ami_id}"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.he-sg.id}"]
   key_name = "he-keypair"
