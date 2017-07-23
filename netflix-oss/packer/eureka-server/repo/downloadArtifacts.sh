6976e4bf12e868cef2afa73c929135704d740f52
Jirawat Uttayaya <admin@apps.jirawat.com>
2017-07-23 13:39:42 -0400

    Start of netflix oss AWS automation

---
+++ netflix-oss/packer/eureka-server/repo/downloadArtifacts.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+curl -O curl http://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.16/bin/apache-tomcat-8.5.16.tar.gz
+curl -O http://repo1.maven.org/maven2/com/netflix/eureka/eureka-server/1.7.0/eureka-server-1.7.0.war
+
+# Download Java 8
+# http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
