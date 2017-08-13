80b460ca17b5c51eeb20528cba94beee91274b79
Jirawat Uttayaya <admin@apps.jirawat.com>
2017-08-13 17:14:50 -0400

    Added docker , java, maven to master-ctrl

--- kubernetes-aws/packer/repo/downloadArtifacts.sh
+++ kubernetes-aws/packer/repo/downloadArtifacts.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 curl -LO https://releases.hashicorp.com/terraform/0.10.0/terraform_0.10.0_linux_amd64.zip
-curl -LO https://releases.hashicorp.com/packer/1.0.3/packer_1.0.3_linux_amd64.zip
+curl -LO https://releases.hashicorp.com/packer/1.0.4/packer_1.0.4_linux_amd64.zip
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 curl -LO https://github.com/kubernetes/kops/releases/download/1.7.0/kops-linux-amd64
+
+curl -LO http://www-eu.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
+
+# Download Java 8 at http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
--- kubernetes-aws/packer/scripts/setupVM.sh
+++ kubernetes-aws/packer/scripts/setupVM.sh
@@ -9,17 +9,37 @@ HASHICORP_BIN=${CTRL_HOME}/hashicorp/bin
 K8S_BIN=${CTRL_HOME}/k8s/bin
 KUBECTL=kubectl
 KOPS=kops-linux-amd64
-PACKER_ZIP=packer_1.0.3_linux_amd64.zip
+PACKER_ZIP=packer_1.0.4_linux_amd64.zip
 TERRAFORM_ZIP=terraform_0.10.0_linux_amd64.zip
+JAVA_ZIP=jdk-8u144-linux-x64.tar.gz
+JAVA_NAME=jdk1.8.0_144
+JAVA_DIR=${CTRL_HOME}/java
+MVN_NAME=apache-maven-3.5.0
+SETUP_GIT="setupGit.sh"
 
+yum install -y git
+yum install -y docker
 
 useradd ${CTRL_USER}
+
+usermod -a -G docker ec2-user
+usermod -a -G docker ${CTRL_USER}
+
 mkdir -p ${HASHICORP_BIN}
 mkdir -p ${K8S_BIN}
 mv ${REPO}/${KOPS} ${K8S_BIN}
 chmod 700 ${K8S_BIN}/${KOPS}
 mv ${REPO}/${KUBECTL} ${K8S_BIN}
 chmod 700 ${K8S_BIN}/${KUBECTL}
+mv ${REPO}/${SETUP_GIT} ${CTRL_HOME}
+chmod 700 ${CTRL_HOME}/${SETUP_GIT}
+
+cd ${CTRL_HOME}
+tar xzpvf ${REPO}/${MVN_NAME}-bin.tar.gz
+
+mkdir -p $JAVA_DIR
+cd $JAVA_DIR
+tar xzpvf ${REPO}/${JAVA_ZIP}
 
 cd ${HASHICORP_BIN}
 unzip ${REPO}/${PACKER_ZIP}
@@ -27,7 +47,8 @@ unzip ${REPO}/${TERRAFORM_ZIP}
 chown 700 *
 
 cat <<EOF >> ${CTRL_HOME}/.bashrc
-export PATH=${HASHICORP_BIN}:${K8S_BIN}:\${PATH}
+export JAVA_HOME=${JAVA_DIR}/${JAVA_NAME}
+export PATH=${HASHICORP_BIN}:${K8S_BIN}:${CTRL_HOME}/${MVN_NAME}/bin:\${JAVA_HOME}/bin:\${PATH}
 EOF
 
 chmod 600 ${CTRL_HOME}/.bashrc
