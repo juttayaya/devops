42e2980c5d330e513f90514efaa54009f7163f9f
Jirawat Uttayaya <admin@apps.jirawat.com>
2017-08-13 00:49:42 -0400

    add DynamoDB for terraform locking

---
+++ kubernetes-aws/terraform/global/dynamodb/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  backend "s3" {
+    bucket = "jirawat-kubectl-io-terraform-state"
+    region = "us-east-2"
+    key = "global/dynamodb/terraform.tfstate"
+#    dynamodb_table = "terraform-netflix-oss-lock"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_dynamodb_table" "dynamodb-jirawat-kubectl-io-terraform-lock" {
+  name = "jirawat-kubectl-io-terraform-lock"
+  hash_key = "LockID"
+  read_capacity = 5
+  write_capacity = 5
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags {
+    Name = "Terraform Lock Table for jirawat.kubectl.io"
+    Org = "jirawat.com"
+    Project = "kubectl.io"
+  }
+}
+ 
