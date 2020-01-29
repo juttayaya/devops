131a3f4c69d5711b791a836ba8f2ab1643b62093
Jirawat Uttayaya <jirawat@apps.jirawat.com>
2020-01-29 15:29:03 -0500

    fix

--- meetup202001/terragrunt/modules/services/project_network_acl_internal/data.tf
+++ meetup202001/terragrunt/modules/services/project_network_acl_internal/data.tf
@@ -1,6 +1,6 @@
 data "aws_vpc" "project_vpc" {
   tags = {
-    Name        = "${project_full_prefix}-vpc"
+    Name        = "${local.project_full_prefix}-vpc"
     project     = var.project
     environment = var.environment
   }
